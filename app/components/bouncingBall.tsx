88db68fa264916a1f0ee6551d931b15aee932758
jordy <minijoo@gmail.com>
2026-05-11 20:17:20 -0400

    api url fix

--- app/components/bouncingBall.tsx
+++ app/components/bouncingBall.tsx
@@ -9,7 +9,11 @@ export default function BouncingBasketball({
   const [ballPos, setBallPos] = useState({ x: 200, y: 50 });
   const [velocity, setVelocity] = useState({ vx: 0, vy: 0 });
   const [isDragging, setIsDragging] = useState(false);
+<<<<<<< HEAD
   const animationRef = useRef(null);
+=======
+  const animationRef = useRef<number>(null);
+>>>>>>> 3072b5a (api url fix)
   const containerRef = useRef<HTMLDivElement>(null);
 
   const BALL_RADIUS = 20;
